41180b8e73d55d6f575790d346a493b786f0ec93
zhang han <LancelotLewis@users.noreply.github.com>
2025-09-15 17:14:16 +0800

    feat: add c (#56)

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,6 @@
 import { a } from "./m-a.mjs";
 import { b } from "./m-b.mjs";
+import { c } from "./m-c.mjs";
 
 function echoA() {
 	console.log(a);
@@ -9,8 +10,13 @@ function echoB() {
 	console.log(b);
 }
 
+function echoC() {
+	console.log(c);
+}
+
 function main() {
 	echoA();
 	echoB();
+	echoC();
 }
 main();
